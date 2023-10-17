dcf22f0bfe5a7d3b52d87ef894322e69e88736a5
schilka <schilka@zuelligpharma.com>
2023-10-17 21:56:30 +0530

    signup api implemented 1.1

--- APIs/api.go
+++ APIs/api.go
@@ -6,5 +6,6 @@ func Apis() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/login/", Login).Methods("POST", "OPTIONS")
 	r.HandleFunc("/createrequest/", CreateRequest).Methods("POST", "OPTIONS")
+	r.HandleFunc("/signUp/", SignUpUser).Methods("POST", "OPTIONS")
 	return r
 }
